5b0839747987cf590fde907f729a971ba5c91afa
Phuc Hoang <hoangmaithienphuc@gmail.com>
2021-07-29 17:21:55 +0700

    add color

--- hello_world/hello_VjpPro/static/js/PROColor.js
+++ hello_world/hello_VjpPro/static/js/PROColor.js
@@ -1,1 +1,7 @@
-alert("Hello");
+document.addEventListener('DOMContentLoaded', function() {
+    setInterval(
+    function () {
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#"+randomColor;
+        },1000);
+});
